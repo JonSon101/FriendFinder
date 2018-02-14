f46cecc6ae2ef53f7a66c8b1d24a76bcaf10de26
Jonathan Landrey Smith <jonathansmith@jonathans-mbp.uconnect.utah.edu>
2018-02-14 15:54:08 -0700

    file

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -1,15 +1,15 @@
-// 3. Your `htmlRoutes.js` file should include two routes:
-
-//    * A GET Route to `/survey` which should display the survey page.
-//    * A default, catch-all route that leads to `home.html` which displays the home page. 
+//Dependencies
 var path = require("path");
 
+//Routing
 module.exports = function(app) {
 
+    //Get survey page
     app.get("/survey", function(req, res) {
         res.sendFile(path.join(__dirname, "../public/survey.html"));
     });
 
+    //Catch-all for home page
     app.get("*", function(req, res) {
         res.sendFile(path.join(__dirname, "../public/home.html"));
     });
