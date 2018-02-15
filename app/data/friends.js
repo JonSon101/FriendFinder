1f2599bb409b34978145825c053a30bf6d6db7e6
Jonathan Landrey Smith <jonathansmith@jonathans-mbp.uconnect.utah.edu>
2018-02-14 18:54:11 -0700

    file

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,20 +1,20 @@
 //Data
 var friendsArray = [
   {
-    "name":"Cow",
-    "photo":"https://upload.wikimedia.org/wikipedia/commons/thumb/0/0c/Cow_female_black_white.jpg/220px-Cow_female_black_white.jpg",
-    "scores[]":[
-        5,
-        1,
-        4,
-        4,
-        5,
-        1,
-        2,
-        5,
-        4,
-        1
-      ]
+    "name":"Ahmed",
+    "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+    "scores":[
+      5,
+      1,
+      4,
+      4,
+      5,
+      1,
+      2,
+      5,
+      4,
+      1
+    ]
   }
 ];
 
